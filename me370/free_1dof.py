59c66b8d005618bff20fd62ac9baf2a9069c0287
Luke A Sargen <lukeasargen@gmail.com>
2021-03-09 01:31:42 -0500

    me370 free 1dof changes

--- me370/free_1dof.py
+++ me370/free_1dof.py
@@ -2,14 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # System
-m = 2  # k
-k = 8  # N/m, stiffness
+m = 10  # kg
+k = 9000  # N/m, stiffness
 c = 0  # Ns/m, damping
 print("System: m={} kg. k={} N/m. c={} Ns/m.".format(m, k, c))
 
-x0 = 3e-3 # m, inital displacement
-v0 = 8e-3 # m/s, intial velocity
+x0 = 0.01 # m, inital displacement
+v0 = -0.7 # m/s, intial velocity
 print("Initial conditions: x0={} m. v0={} m/s..".format(x0, v0))
+
+sim_length = 1 # seconds
+show_graph = True
 
 omega_n = np.sqrt(k/m)  # natural frequency
 print("Natural Frequency = {:.4f} rad/s.".format(omega_n))
@@ -21,17 +24,14 @@ print("Phase = {:.4f} rad.".format(phase))
 # Solution
 x_t = lambda t: amplitude*np.sin(omega_n*t + phase)
 
-sim_length = 8 # seconds
-steps = np.linspace(0, sim_length, sim_length*100)
+steps = np.linspace(0, sim_length, sim_length*250)
 
-plt.plot(steps, x_t(steps))
-
-plt.grid()
-# plt.grid(b=True, which='major', color='#666666', linestyle='-')
-
-plt.xlabel('Seconds')
-plt.ylabel('Displacement (m)')
-
-plt.show()
+if show_graph:
+    plt.plot(steps, x_t(steps))
+    plt.grid()
+    # plt.grid(b=True, which='major', color='#666666', linestyle='-')
+    plt.xlabel('Seconds')
+    plt.ylabel('Displacement (m)')
+    plt.show()
 
 
