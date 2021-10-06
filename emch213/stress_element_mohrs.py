8a44d48f2d78718972fcf7f9703241bece57e38e
Luke A Sargen <lukeasargen@gmail.com>
2021-10-06 10:33:33 -0400

    Add emch312 lab3

--- emch213/stress_element_mohrs.py
+++ emch213/stress_element_mohrs.py
@@ -2,156 +2,174 @@ import argparse
 import numpy as np
 
 parser = argparse.ArgumentParser(description="Shear Tranforms and Mohr's Circle")
-parser.add_argument('--sx', type=float, default=0.0, metavar="Stress in X axis")
-parser.add_argument('--sy', type=float, default=0.0, metavar="Stress in Y axis")
-parser.add_argument('--txy', type=float, default=0.0, metavar="Shear Stress.")
-parser.add_argument('--deg', type=float, default=0.0, metavar="Degrees. Cut plane angle positive in ccw direction from X axis.")
-parser.add_argument('--v', action='store_true')
+parser.add_argument('--sx', type=float, default=0.0, help="Stress in X axis")
+parser.add_argument('--sy', type=float, default=0.0, help="Stress in Y axis")
+parser.add_argument('--txy', type=float, default=0.0, help="Shear Stress.")
+parser.add_argument('--deg', type=float, default=0.0, help="Degrees. Cut plane angle positive in ccw direction from X axis.")
+parser.add_argument('-strain', action='store_true', help="Inputs are strain values.")
+parser.add_argument('--E', type=float, default=0.0, help="Elastic Modulus.")
+parser.add_argument('--v', type=float, default=0.0, help="Poisson's Ratio.")
+parser.add_argument('-viz', action='store_true', help="Visualize Stress Element.")
 args = parser.parse_args()
 
 sigma_x = args.sx
 sigma_y = args.sy
 tau_xy = args.txy
 theta_deg = args.deg  # degrees, ccw is positive
-visualize = args.v
 
-print("σ x = {}".format(sigma_x))
-print("σ y = {}".format(sigma_y))
-print("τ xy = {}".format(tau_xy))
-print("θ = {}".format(theta_deg))
+if args.strain:
+    tau_xy = tau_xy/2  # MOHR CIRCLE USES HALF SHEAR FOR STRAIN
 
+normal_symbol = "ε" if args.strain else "σ"
+shear_symbol = "γ" if args.strain else "τ"
+value_unit = "Strain" if args.strain else "Stress"
 
-print("\nStress transformation")
+if args.strain:
+    print("INPUTS ARE STRAIN")
+else:
+    print("INPUTS ARE STRESS")
+
+print(f"{normal_symbol}_x = {sigma_x:e}")
+print(f"{normal_symbol}_y = {sigma_y:e}")
+print(f"{shear_symbol}_xy = {tau_xy:e}")
+print(f"θ = {theta_deg}")
 
 tau_max_in_plane = np.sqrt( (0.5*(sigma_x-sigma_y))**2 + tau_xy**2 )
 sigma_avg = 0.5*(sigma_x+sigma_y)
 
+if args.deg != 0.0:
+    print(f"\n{value_unit} transformation")
+    print(f"{value_unit} at {theta_deg} degrees.")
+    sigma_x_prime = (0.5*(sigma_x+sigma_y)) + (0.5*(sigma_x-sigma_y)*np.cos(2*np.radians(theta_deg))) + (tau_xy*np.sin(2*np.radians(theta_deg)))
+    sigma_y_prime = (0.5*(sigma_x+sigma_y)) - (0.5*(sigma_x-sigma_y)*np.cos(2*np.radians(theta_deg))) - (tau_xy*np.sin(2*np.radians(theta_deg)))
+    tau_xy_prime = (-0.5*(sigma_x-sigma_y)*np.sin(2*np.radians(theta_deg)) + (tau_xy*np.cos(2*np.radians(theta_deg))))
+    print(f"{normal_symbol}_x' = {sigma_x_prime:e}")
+    print(f"{normal_symbol}_y' = {sigma_y_prime:e}")
+    print(f"{shear_symbol}_x'y' = {tau_xy_prime:e}")
 
-print("Stresses at {} degrees.".format(theta_deg))
-sigma_x_prime = (0.5*(sigma_x+sigma_y)) + (0.5*(sigma_x-sigma_y)*np.cos(2*np.radians(theta_deg))) + (tau_xy*np.sin(2*np.radians(theta_deg)))
-sigma_y_prime = (0.5*(sigma_x+sigma_y)) - (0.5*(sigma_x-sigma_y)*np.cos(2*np.radians(theta_deg))) - (tau_xy*np.sin(2*np.radians(theta_deg)))
-tau_xy_prime = (-0.5*(sigma_x-sigma_y)*np.sin(2*np.radians(theta_deg)) + (tau_xy*np.cos(2*np.radians(theta_deg))))
-print("σx' = {}".format(sigma_x_prime))
-print("σy' = {}".format(sigma_y_prime))
-print("τx'y' = {}".format(tau_xy_prime))
 
-
-print("\nPrincipal Stress")
-print("Plane with no shear stress.")
+print(f"\nPrincipal {value_unit}, Plane with no Shear {value_unit}.")
 
 # print("in_top :", (2*tau_xy))
 # print("in_bot :", (sigma_x-sigma_y))
 # print("in_atan :", (2*tau_xy) / (sigma_x-sigma_y)) 
-theta_p1_temp = 0.5 * np.degrees( np.arctan2(  (2*tau_xy) , (sigma_x-sigma_y) ) )
-# theta_p1_temp = 0.5 * np.degrees( np.arctan( (2*tau_xy) / (sigma_x-sigma_y) ) )
+theta_p1 = 0.5 * np.degrees( np.arctan2(  (2*tau_xy) , (sigma_x-sigma_y) ) )
+# theta_p1 = 0.5 * np.degrees( np.arctan( (2*tau_xy) / (sigma_x-sigma_y) ) )
 
 # tan(2a) = tan(2a+180) = b
 # hence, a = arctan(a)/2 - 90
-theta_p2_temp = ( theta_p1_temp - 90 )
-while theta_p2_temp < -90:
-    theta_p2_temp += 180
+theta_p2 = ( theta_p1 - 90 )
+while theta_p2 < -90:
+    theta_p2 += 180
 
-if abs(theta_p1_temp) > abs(theta_p2_temp):
-    theta_p1 = theta_p2_temp
-    theta_p2 = theta_p1_temp
-else:
-    theta_p1 = theta_p1_temp
-    theta_p2 = theta_p2_temp
+print(f"Orientation of Principal Planes of {value_unit}")
 
-print("Orientation of Principal Planes of Stress")
-
-print("θ p1={:.6f}".format(theta_p1))
+print(f"θ_p1={theta_p1:.6f} deg")
 sigma_x_prime_p1 = (0.5*(sigma_x+sigma_y)) + (0.5*(sigma_x-sigma_y)*np.cos(2*np.radians(theta_p1))) + (tau_xy*np.sin(2*np.radians(theta_p1)))
 sigma_y_prime_p1 = (0.5*(sigma_x+sigma_y)) - (0.5*(sigma_x-sigma_y)*np.cos(2*np.radians(theta_p1))) - (tau_xy*np.sin(2*np.radians(theta_p1)))
 tau_xy_prime1 = (-0.5*(sigma_x-sigma_y)*np.sin(2*np.radians(theta_p1)) + (tau_xy*np.cos(2*np.radians(theta_p1))))
-print("σx' p1={:.6f}, σy' p1={:.6f}, τx'y' p1={:.6f}".format(sigma_x_prime_p1, sigma_y_prime_p1, tau_xy_prime1))
+print(f"p1: {normal_symbol}_x'={sigma_x_prime_p1:e}, {normal_symbol}_y'={sigma_y_prime_p1:e}, {shear_symbol}_x'y'={tau_xy_prime1:e}")
 
-print("θ p2={:.6f}".format(theta_p2))
+print(f"θ_p2={theta_p2:.6f} deg")
 sigma_x_prime_p2 = (0.5*(sigma_x+sigma_y)) + (0.5*(sigma_x-sigma_y)*np.cos(2*np.radians(theta_p2))) + (tau_xy*np.sin(2*np.radians(theta_p2)))
 sigma_y_prime_p2 = (0.5*(sigma_x+sigma_y)) - (0.5*(sigma_x-sigma_y)*np.cos(2*np.radians(theta_p2))) - (tau_xy*np.sin(2*np.radians(theta_p2)))
 tau_xy_prime2 = (-0.5*(sigma_x-sigma_y)*np.sin(2*np.radians(theta_p2)) + (tau_xy*np.cos(2*np.radians(theta_p2))))
-print("σx' p2={:.6f}, σy' p2={:.6f}, τx'y' p2={:.6f}".format(sigma_x_prime_p2, sigma_y_prime_p2, tau_xy_prime2))
+print(f"p2: {normal_symbol}_x'={sigma_x_prime_p2:e}, {normal_symbol}_y'={sigma_y_prime_p2:e}, {shear_symbol}_x'y'={tau_xy_prime2:e}")
 
 
-print("\nMaximum In-Plane Shear Stress")
+print(f"\nMaximum In-Plane Shear {value_unit}")
 
-theta_s1_temp = 0.5 * np.degrees( np.arctan2( (sigma_x-sigma_y) , (-2*tau_xy) ) )
-# theta_s1_temp = 0.5 * np.degrees( np.arctan( (sigma_x-sigma_y) / (-2*tau_xy) ) )
+theta_s1 = 0.5 * np.degrees( np.arctan2( (sigma_x-sigma_y) , (-2*tau_xy) ) )
+# theta_s1 = 0.5 * np.degrees( np.arctan( (sigma_x-sigma_y) / (-2*tau_xy) ) )
 
 # tan(2a) = tan(2a+180) = b
 # hence, a = arctan(a)/2 - 90
-theta_s2_temp = ( theta_s1_temp - 90 )
-while theta_s2_temp < -90:
-    theta_s2_temp += 180
+theta_s2 = ( theta_s1 - 90 )
+while theta_s2 < -90:
+    theta_s2 += 180
 
-if abs(theta_s1_temp) > abs(theta_s2_temp):
-    theta_s1 = theta_s2_temp
-    theta_s2 = theta_s1_temp
-else:
-    theta_s1 = theta_s1_temp
-    theta_s2 = theta_s2_temp
-
-print("θ s1={:.6f}, θ s2={:.6f}".format(theta_s1, theta_s2))
+print(f"θ_s1={theta_s1:.6f} deg. θ_s2={theta_s2:.6f} deg.")
 
 if theta_s1 > 0:
     tau_max_in_plane *= -1
 
-print("τ max in plane = {}. **Sign matters.\nUse '--v' argument to visualize the In-Plane Shear.".format(tau_max_in_plane))
-print("σ avg = {}".format(sigma_avg))
+
+print(f"{'γ/2' if args.strain else 'τ'} max in plane = {tau_max_in_plane:e}. **Sign matters.")
+print("Use '-viz' argument to visualize the In-Plane Shear.")
+print(f"{normal_symbol} avg = {sigma_avg:e}")
 
 
-print("\nAbsolute Maximum Shear Stress")
+print(f"\nAbsolute Maximum Shear {value_unit}")
 
-sigma_1_temp = sigma_avg + abs(tau_max_in_plane)
-sigma_2_temp = sigma_avg - abs(tau_max_in_plane)
+sigma_1 = sigma_avg + abs(tau_max_in_plane)
+sigma_2 = sigma_avg - abs(tau_max_in_plane)
 
-if abs(sigma_1_temp) > abs(sigma_2_temp):
-    sigma_1 = sigma_1_temp
-    sigma_2 = sigma_2_temp
-else:
-    sigma_1 = sigma_2_temp
-    sigma_2 = sigma_1_temp
+# if abs(sigma_1) < abs(sigma_2):
+#     sigma_1, sigma_2 = sigma_2, sigma_1
 
-print("σ1={:.6f}, σ2={:.6f}".format(sigma_1, sigma_2))
+print(f"{normal_symbol}_1={sigma_1:e}, {normal_symbol}_2={sigma_2:e}")
 
 if sigma_1 * sigma_2 > 0 :  # same sign
-    print("σ1 and σ2 have the same sign.")
-    print("σ3 is out of plane.")
+    print(f"{normal_symbol}_1 and {normal_symbol}_2 have the same sign.")
+    print(f"{normal_symbol}_3 is out of plane.")
     if abs(sigma_1) > abs(sigma_2):
-        print("σ1 is larger.")
+        # print(f"{normal_symbol}_1 is larger.")
         tau_abs_max = 0.5*sigma_1
     else:
-        print("σ2 is larger.")
+        # print(f"{normal_symbol}_2 is larger.")
         tau_abs_max = 0.5*sigma_2
 else:  # opposite sign
-    print("σ1 and σ2 have the opposite sign.")
-    print("maximum shear equals tau max in-plane.")
+    print(f"{normal_symbol}_1 and {normal_symbol}_2 have the opposite sign.")
+    print(f"maximum shear equals {'γ/2' if args.strain else 'τ'} max in-plane.")
     tau_abs_max = 0.5*(sigma_1-sigma_2)
 
-print("τ abs max = {}".format(tau_abs_max))
+print(f"{'γ/2' if args.strain else 'τ'} abs max = {tau_abs_max:e}")
 
 
 print("\nMohr's Circle")
-
-
-print("X point = ({:.6f}, {:.6f})".format(sigma_x, -tau_xy))
+print(f"X point = ({sigma_x:e}, {tau_xy:e})")
 print("Rotate X to the closest X axis.")
-print("2 θp = {} from the σ+ axis.".format( np.degrees( np.arctan2( -tau_xy, sigma_x-sigma_avg )) ) )
-print("Y point = ({:.6f}, {:.6f})".format(sigma_y, tau_xy))
-print("Center (σ avg) = ({:.6f}, 0)".format(sigma_avg))
-print("Radius (τ max) = {:.6f}".format(abs(tau_max_in_plane)))
+print(f"2 θp = {np.degrees(np.arctan2(-tau_xy, sigma_x-sigma_avg))} from the σ+ axis.")
+print(f"Y point = ({sigma_y:e}, {-tau_xy:e})")
+print(f"Center ({normal_symbol} avg) = ({sigma_avg:e}, 0)")
+print(f"Radius ({'γ/2' if args.strain else 'τ'} max) = {abs(tau_max_in_plane):e}")
 
 
-print("\n*** REMINDERS ***")
-print(" |θp| + |θs| = 45 degrees.")
-print(" σ3 is always at zero. Out of plane constant.")
+if not args.strain:
+    print("\n*** REMINDERS ***")
+    print(" |θp| + |θs| = 45 degrees.")
+    print(" σ3 is always at zero. Out of plane constant.")
+
+if args.strain:
+    if args.E and args.v:
+        print("\nPrincipal Stress from Strain, Hooke's Law for plane stress")
+        # calcuate the sigma_# from the stresses, bc rn sigma_# is the strain input
+        ev = args.E/(1-(args.v*args.v))
+        sigma_1, sigma_2 = ev*(sigma_1 + args.v*sigma_2), ev*(sigma_2 + args.v*sigma_1)
+        print(f"σ_1={sigma_1:e}")
+        print(f"σ_2={sigma_2:e}")
+
+        if sigma_1 * sigma_2 > 0 :  # same sign
+            print("σ_1 and σ_2 have the same sign.")
+            print("σ_3 is out of plane.")
+            if abs(sigma_1) > abs(sigma_2):
+                # print("σ_1 is larger.")
+                tau_abs_max = 0.5*sigma_1
+            else:
+                # print("σ_2 is larger.")
+                tau_abs_max = 0.5*sigma_2
+        else:  # opposite sign
+            print("σ_1 and σ_2 have the opposite sign.")
+            print("maximum shear equals τ max in-plane.")
+            tau_abs_max = 0.5*(sigma_1-sigma_2)
+        print(f"τ abs max = {tau_abs_max:e}")
 
 
-if visualize:
+if args.viz:
     from PIL import Image, ImageDraw, ImageFont
 
     # Size and position
-    size = (1000, 1000)
+    size = (1200, 1000)
     scale = 90
     font_size = int(scale / 4.8)
     # print("font_size :", font_size)
@@ -221,7 +239,7 @@ if visualize:
             arr = rotate_2d(arrow_points(scale), 180+angle) + rotate_2d([-scale*1.25, 0], 90+angle, center)
             draw.polygon(xy=tuple([tuple(x) for x in arr]), fill=arrow_color, outline=arrow_outline)
 
-            draw.text(xy=tuple(rotate_2d([-scale*text_move, 0], 225+angle, center)), text="txy={:.2f}".format(txy), fill=text_color, anchor="mm", font=font)
+            draw.text(xy=tuple(rotate_2d([-scale*text_move, 0], 225+angle, center)), text="txy={:e}".format(txy), fill=text_color, anchor="mm", font=font)
 
         # normal arrows
         if sx != 0:
@@ -229,20 +247,20 @@ if visualize:
             draw.polygon(xy=tuple([tuple(x) for x in arr]), fill=arrow_color, outline=arrow_outline)
             arr = rotate_2d(arrow_points(scale*0.6), 180+angle) + rotate_2d([-scale*1.6, 0], angle, center)
             draw.polygon(xy=tuple([tuple(x) for x in arr]), fill=arrow_color, outline=arrow_outline)
-            draw.text(xy=tuple(rotate_2d([-scale*text_move, 0], 180+angle, center)), text="sx={:.2f}".format(sx), fill=text_color, anchor="mm", font=font)
+            draw.text(xy=tuple(rotate_2d([-scale*text_move, 0], 180+angle, center)), text="sx={:e}".format(sx), fill=text_color, anchor="mm", font=font)
 
         if sy != 0:
             arr = rotate_2d(arrow_points(scale*0.6), 90+angle) + rotate_2d([scale*1.6, 0], 90+angle, center)
             draw.polygon(xy=tuple([tuple(x) for x in arr]), fill=arrow_color, outline=arrow_outline)
             arr = rotate_2d(arrow_points(scale*0.6), -90+angle) + rotate_2d([-scale*1.6, 0], 90+angle, center)
             draw.polygon(xy=tuple([tuple(x) for x in arr]), fill=arrow_color, outline=arrow_outline)
-            draw.text(xy=tuple(rotate_2d([-scale*text_move, 0], -90+angle, center)), text="sy={:.2f}".format(sy), fill=text_color, anchor="mm", font=font)
+            draw.text(xy=tuple(rotate_2d([-scale*text_move, 0], -90+angle, center)), text="sy={:e}".format(sy), fill=text_color, anchor="mm", font=font)
 
         # Title
-        draw.text(xy=tuple(rotate_2d([-scale*2.25, -scale*2.25], 0, center)), text="{}, θ={:.2f}.".format(title, angle), fill=text_color, anchor="mm", font=font)
+        draw.text(xy=tuple(rotate_2d([0, -scale*2.35], 0, center)), text="{}, θ={:.2f}.".format(title, angle), fill=text_color, anchor="mm", font=font)
 
     # Draw element
-    draw_element(center=input_center, scale=scale, title="Input", sx=sigma_x, sy=sigma_y, txy=tau_xy)
+    draw_element(center=input_center, scale=scale, title="Input", angle=args.deg, sx=sigma_x, sy=sigma_y, txy=tau_xy)
 
     # Draw pricinple axis, no shear
     draw_element(center=principal_center, scale=scale, title="Principal Plane (No Shear)", angle=theta_p1, sx=sigma_x_prime_p1, sy=sigma_y_prime_p1)
