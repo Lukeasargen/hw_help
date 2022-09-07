5faebb932dd642d619473ebea93af87dec1ae468
Luke A Sargen <lukeasargen@gmail.com>
2022-09-07 10:29:46 -0400

    me400 hw1 check 1a

--- me400/hw_01_1.py
+++ me400/hw_01_1.py
@@ -8,7 +8,7 @@ b = 5.981e-2
 c = -3.501e-5
 d = 7.469e-9
 
-# cp = a + bT + cT^2 + dT^3, KJ.Kmol*K
+# cp = a + bT + cT^2 + dT^3, KJ/Kmol*K
 def f(T):
     return a + b*T + c*T**2 + d*T**3
 
@@ -23,10 +23,17 @@ m_dot = 50  # kg/s
 
 T = np.linspace(273, 1800, 100)
 cp = f(T)/M_CO2
-# print(cp)
+print(cp)
 
 cp_int = g(T2)-g(T1)  # kJ/Kmol
 print("Integral", cp_int)
 W_cv = m_dot/M_CO2 * cp_int  # kWatts
 print("W_cv", W_cv)
 
+# Check part A
+t_avg = 0.5*(T1+T2)  # K
+print(f"{t_avg=}")
+cp_avg = f(t_avg)/M_CO2  # KJ/Kg*K
+print(f"{cp_avg=}")
+W_cv = m_dot*cp_avg*(T1-T2)  # kWatts
+print(f"{W_cv=}")
